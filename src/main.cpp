78fc50f01b2aabed49b044180b1304c0c32258ac
Ayaan Ahmed <aayan.shazim@gmail.com>
2025-07-21 15:45:44 +0500

    fn to split paragraphs

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,38 @@
 
 using namespace std;
 
+//split test paragraphs by blank lines 
+vector<string> splitParagraphs (const string& filename){
+    ifstream file(filename);
+    vector<string> paragraphs;
+    string line, paragraph;
+
+    if (!file.is_open()) {
+        cerr << "Error opening file: " << filename << endl;
+        return paragraphs;
+    }
+
+    while (getline(file, line)) {
+        if (line.empty()) {
+            if (!paragraph.empty()) {
+                paragraphs.push_back(paragraph);
+                paragraph.clear();
+            }
+        } else {
+            if (!paragraph.empty()) {
+                paragraph += "\n";
+            }
+            paragraph += line;
+        }
+    }
+
+    if (!paragraph.empty()) {
+        paragraphs.push_back(paragraph);
+    }
+    file.close();
+    return paragraphs;
+    
+}
 int main() {
     
     return 0;
