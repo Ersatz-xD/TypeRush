48b0776a7bf48ce65a4a29179f203424dd7f6f7f
Ayaan Ahmed <aayan.shazim@gmail.com>
2025-07-22 12:07:23 +0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,14 @@
 #include <thread>
 #include <chrono>
 #include <atomic>
+#include <conio.h>
+#include <filesystem>
+
+
 
 
 using namespace std;
+namespace fs = std::filesystem;
 
 atomic<bool> timerStarted(false);
 atomic<bool> timesUp(false);
@@ -20,7 +25,7 @@ void startTimer(int duration) {
     while (remaining> 0 && !timesUp)
     {
         if (timerStarted){
-            cout << "\rTime left: " << remaining << " seconds";
+           // cerr << "\rTime left: " << remaining << " seconds";
             this_thread::sleep_for(chrono::seconds(1));
             remaining--;
         }
@@ -30,7 +35,7 @@ void startTimer(int duration) {
     }
     
     timesUp = true;
-    cout << "\nTime's up!" << endl;
+    cerr << "\nTime's up!" << endl;
 }
 
 
@@ -72,15 +77,53 @@ vector<string> loadParagraphs (const string& filename){
 
 int main() {
     srand(time(0)); 
-    vector<string> paragraphs = loadParagraphs("..\\testParagraphs\\paragraphs.txt");
+    fs::path filePath = fs::path(__FILE__).parent_path() / "../testParagraphs/paragraphs.txt";
+    vector<string> paragraphs = loadParagraphs(filePath.string());
+
     if (paragraphs.empty()) {
         cerr << "No paragraphs found in the file." << endl;
         return 1;
     }
 
     int randomIndex = rand() % paragraphs.size();
-    string randomParagraph = paragraphs[randomIndex];      
-    cout << "Randomly selected paragraph:\n" << randomParagraph << endl;    
+    string randomParagraph = paragraphs[randomIndex];
+    string userInput;      
+    
+    cout << "Type the following paragraph as fast as you can:\n" << randomParagraph << endl;
+    cout << "Start typing below. Timer will start when you begin typing...\n\n";
 
+
+    thread timerThread(startTimer, 10); // Start a 10 second timer
+    
+
+    // Wait until user presses any key
+    while (!_kbhit()) {
+        this_thread::sleep_for(chrono::milliseconds(10));
+    }
+    timerStarted = true; 
+
+    while(!timesUp && userInput.length() < randomParagraph.length()) {
+       if (_kbhit()) {
+        char ch = _getch();
+
+        if (ch == 8) { // Backspace key
+            if (!userInput.empty()) {
+                userInput.pop_back(); // Remove last character
+
+                // Move cursor back, overwrite with space, then move back again
+                cout << "\b \b";
+            }
+        } else {
+            cout << ch;
+            userInput += ch;
+        }
+    }
+    this_thread::sleep_for(chrono::milliseconds(10));
+    }
+
+    timesUp = true; 
+    timerThread.join(); 
+
+    cout << "You typed: " << userInput << endl;
     return 0;
 }
